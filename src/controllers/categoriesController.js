1673c9bb11f4219d7b856ddc0a608325c79adcb5
Anastasiia Anisimova <gorgona1597531597@gmail.com>
2025-12-12 18:52:11 +0100

    Remove error handling from getAllCategories

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,13 +1,9 @@
 import { Category } from '../models/category.js';
 
 export const getAllCategories = async (req, res) => {
-  try {
     const categories = await Category.find();
     if (categories.length <= 0) {
       return res.status(400).json({ error: 'Not found categories' });
     }
     res.status(200).json(categories);
-  } catch {
-    return res.status(500).json('Server error while fetching categories');
-  }
 };
