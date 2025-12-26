200dbb79776ef2988ba1fac990ef31cc2682dc5f
Beowulf3 <vk3volkov@gmail.com>
2025-12-26 14:40:24 +0200

    add sort for categories by alphabet

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,9 +1,11 @@
 import { Category } from '../models/category.js';
 
 export const getAllCategories = async (req, res) => {
-    const categories = await Category.find();
-    if (categories.length <= 0) {
-      return res.status(400).json({ error: 'Not found categories' });
-    }
-    res.status(200).json(categories);
+  const categories = (await Category.find()).sort((a, b) =>
+    a.title.localeCompare(b.title, 'uk-UA'),
+  );
+  if (categories.length <= 0) {
+    return res.status(400).json({ error: 'Not found categories' });
+  }
+  res.status(200).json(categories);
 };
